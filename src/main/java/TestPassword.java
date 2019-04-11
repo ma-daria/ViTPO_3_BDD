b90c2d827bf8f2b70b3d67664ecc5b2dd6457d83
ma-daria <marina.dasha18@yandex.ru>
2019-04-11 19:53:43 +0700

    написание теста для 1й спецификации

--- src/main/java/TestPassword.java
+++ src/main/java/TestPassword.java
@@ -2,33 +2,40 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class TestPassword {
 
+    Password classPassword;
+    String servis;
+    String password;
+    String result;
+
     @cucumber.api.java.en.Given("^I Have class password$")
     public void iHaveClassPassword() {
+        classPassword = new Password();
     }
 
     @When("^I have entered \"([^\"]*)\" as servis operand$")
     public void iHaveEnteredAsServisOperand(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        servis = arg0;
     }
 
     @And("^I have entered \"([^\"]*)\" as password operand$")
     public void iHaveEnteredAsPasswordOperand(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        password = arg0;
     }
 
     @And("^I add password$")
     public void iAddPassword() {
-
+        result = classPassword.addPassword(servis, password);
     }
 
     @Then("^The result should be \"([^\"]*)\"$")
     public void theResultShouldBe(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Assert.assertEquals(result, arg0);
     }
 }
