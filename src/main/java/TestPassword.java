21979604a7c7d03bfa4fc26ed209afe8b5b169e4
ma-daria <marina.dasha18@yandex.ru>
2019-04-12 01:19:17 +0700

    Спецификация 0.8. Возврат строки. Угаданна 1а буква| добавление метода

--- src/main/java/TestPassword.java
+++ src/main/java/TestPassword.java
@@ -71,4 +71,10 @@ public class TestPassword {
         // Write code here that turns the phrase above into concrete actions
         result = classPassword.getString(arg0);
     }
+
+    @And("^I chek letter \"([^\"]*)\" from \"([^\"]*)\"$")
+    public void iChekLetterFrom(String arg0, String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        classPassword.chekLetter(arg0, arg1);
+    }
 }
